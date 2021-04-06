2822f1a6ce025c44105dd55a8e386ca4e69d95c8
faruoqi <rezafaruoqi@gmail.com>
2021-04-06 19:20:52 +0700

    future based CollectorService

--- src/main/java/services/CollectorService.java
+++ src/main/java/services/CollectorService.java
@@ -1,18 +1,19 @@
 package services;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.predicate.ResponsePredicate;
 import io.vertx.ext.web.codec.BodyCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class CollectorService extends AbstractVerticle {
     private final Logger logger = LoggerFactory.getLogger(CollectorService.class);
@@ -27,6 +28,51 @@ public class CollectorService extends AbstractVerticle {
     }
 
     private void handleRequest(HttpServerRequest request) {
+        CompositeFuture.all(
+                fetchTemperature(3000),
+                fetchTemperature(3001),
+                fetchTemperature(3002)
+        ).flatMap(this::sendToSnapshot)
+        .onSuccess(data -> request
+                .response()
+                .putHeader("Content-Type", "application/json")
+                .end(data.encode()))
+        .onFailure(err -> {
+            logger.error("something went wrong",err);
+            request.response().setStatusCode(500).end();
+        });
+
+    }
+
+    private Future<JsonObject> sendToSnapshot(CompositeFuture temps) {
+        List<JsonObject> tempData = temps.list();
+        JsonObject data = new JsonObject()
+                .put("data",new JsonArray()
+                .add(tempData.get(0))
+                .add(tempData.get(1))
+                .add(tempData.get(2)));
+
+        return webClient.post(4000,"localhost","/")
+                .expect(ResponsePredicate.SC_SUCCESS)
+                .sendJson(data)
+                .map(bufferHttpResponse -> data);
+
+    }
+
+    private Future<JsonObject> fetchTemperature(int port){
+        return webClient
+                .get(port,"localhost","/")
+                .expect(ResponsePredicate.SC_SUCCESS)
+                .as(BodyCodec.jsonObject())
+                .send()
+                .map(HttpResponse::body);
+    }
+
+
+
+
+    /*
+    private void handleRequestOld(HttpServerRequest request) {
         List<JsonObject> responses = new ArrayList<>();
         AtomicInteger counter = new AtomicInteger(0);
         for (int i = 0; i < 3;i++){
@@ -48,7 +94,11 @@ public class CollectorService extends AbstractVerticle {
         }
     }
 
-    private void sendToSnapshot(HttpServerRequest request, JsonObject data) {
+     */
+
+
+    /*
+    private void sendToSnapshotOld(HttpServerRequest request, JsonObject data) {
         webClient.post(4000,"localhost","/")
                 .expect(ResponsePredicate.SC_SUCCESS)
                 .sendJsonObject(data,ar -> {
@@ -60,6 +110,8 @@ public class CollectorService extends AbstractVerticle {
                     }
                 });
     }
+
+     */
 
     private void sendResponse(HttpServerRequest request, JsonObject data) {
         request.response()
